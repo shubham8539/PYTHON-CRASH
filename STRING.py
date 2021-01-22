7028f907ab9ce46d6e03767b85a5e468395b82d8
Shubham <48403811+shubham8539@users.noreply.github.com>
2021-01-22 17:14:09 +0530

    Add files via upload

---
+++ STRING.py
@@ -0,0 +1,27 @@
+Python 3.7.2 (tags/v3.7.2:9a3ffc0492, Dec 23 2018, 22:20:52) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> ###STRING
+>>> word="amazing"
+>>> word[1:6:2]
+'mzn'
+>>> word[0:6:1]
+'amazin'
+>>> ###String function
+>>> s="My name is Shubham I am studying in HIT haldia"
+>>> len(s)
+46
+>>> s.endswith('haldia')
+True
+>>> s.count('m')
+3
+>>> s.capitalize()
+'My name is shubham i am studying in hit haldia'
+>>> s.find('I')
+19
+>>> s.replace("HIT","IIT")
+'My name is Shubham I am studying in IIT haldia'
+>>> s.lower()
+'my name is shubham i am studying in hit haldia'
+>>> s.upper()
+'MY NAME IS SHUBHAM I AM STUDYING IN HIT HALDIA'
+>>> 
